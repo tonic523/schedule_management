208d2a648d579d08781f75df4a23c10f9c9ce1fb
ChoiWooseok <diddmlvkf@gmail.com>
2021-06-08 18:38:57 +0900

    Create: 프로젝트 초기 셋팅

---
+++ b2tech_intern_20/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for b2tech_intern_20 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'b2tech_intern_20.settings')
+
+application = get_asgi_application()
---
+++ punctuality/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PunctualityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'punctuality'
