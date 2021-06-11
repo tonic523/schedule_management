6307398890ac856f570df23fedc5582b973d201a
tonic523 <diddmlvkf@gmail.com>
2021-06-11 14:50:31 +0900

    Refactor: 모델링 수정

--- schedules/models.py
+++ schedules/models.py
@@ -1,27 +1,42 @@
-from django.db import models
+from django.utils              import timezone
+from django.db                 import models
 from django.db.models.deletion import CASCADE
 
-class Schedule(models.Model):
-    user = models.ForeignKey('users.User', on_delete=CASCADE)
-    act = models.ForeignKey('Act', on_delete=CASCADE)
-    hours = models.IntegerField(default=0)
-    start_at = models.DateTimeField()
-    end_at = models.DateTimeField()
-    status = models.ForeignKey('ScheduleStatus', on_delete=CASCADE)
-
+class Work(models.Model):
+    STATUS = (
+        (1, "출근 전"),
+        (2, "출근"),
+        (3, "지각"),
+        (4, "결석"),
+        (5, "퇴근"),
+        (6, "야근"),
+        (7, "휴일근무")
+        )
+    user      = models.ForeignKey('users.User', on_delete=CASCADE)
+    status    = models.IntegerField(default=1, choices=STATUS)
+    start_at  = models.DateTimeField(default=timezone.now())
+    end_at    = models.DateTimeField(default=None, null=True)
+    
     class Meta:
         db_table = 'schedules'
 
-class Act(models.Model):
-    name = models.CharField(max_length=64)
-    start_at = models.TimeField()
+class Leave(models.Model):
+    STATUS = (
+        (1, "신청"),
+        (2, "결재"),
+        (3, "반려")
+        )
+    LEAVE_TYPE = (
+        ("연차", 1),
+        ("공가", 0),
+        ("경조", 0)
+        )
+    user     = models.ForeignKey('users.User', on_delete=CASCADE)
+    start_at = models.DateTimeField()
+    end_at   = models.DateTimeField()
+    status   = models.IntegerField(default=1, choices=STATUS)
+    text     = models.TextField()
+    type     = models.CharField(default="연차", choices=LEAVE_TYPE, max_length=32)
 
     class Meta:
-        db_table = 'acts'
-
-class ScheduleStatus(models.Model):
-    name = models.CharField(max_length=64)
-
-    class Meta:
-        db_table = 'shcedule_status'
-
+        db_table = 'leaves'
--- users/models.py
+++ users/models.py
@@ -21,6 +21,11 @@ class CustomUserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
+    WORK_TYPE = (
+        (1, "08:00"),
+        (2, "09:00"),
+        (3, "10:00") 
+        )
     email               = models.EmailField(max_length=255, unique=True)
     name                = models.CharField(max_length=64)
     phone_number        = models.CharField(max_length=64)
@@ -29,7 +34,7 @@ class User(AbstractBaseUser):
     date_of_join        = models.DateField()
     employee_number     = models.CharField(max_length=64)
     roles               = models.ManyToManyField('Role', through='UserRole', related_name='managers')
-    allowances          = models.ManyToManyField('schedules.Act', through='Allowance', related_name='employees')
+    work_type           = models.IntegerField(default=2, choices=WORK_TYPE)
 
     class Meta:
         db_table = 'users'
@@ -66,33 +71,3 @@ class UserRole(models.Model):
     class Meta:
         db_table = 'users_roles'
 
-class Allowance(models.Model):
-    act  = models.ForeignKey('schedules.Act', on_delete=CASCADE)
-    user = models.ForeignKey(User, on_delete=CASCADE)
-
-    class Meta:
-        db_table = 'allowances'
-
-class Leave(models.Model):
-    user     = models.ForeignKey(User, on_delete=CASCADE)
-    start_at = models.DateTimeField()
-    end_at   = models.DateTimeField()
-    status   = models.ForeignKey('LeaveStatus', on_delete=CASCADE)
-    text     = models.TextField()
-    type     = models.ForeignKey('LeaveType', on_delete=CASCADE)
-
-    class Meta:
-        db_table = 'leaves'
-
-class LeaveStatus(models.Model):
-    name = models.CharField(max_length=64)
-
-    class Meta:
-        db_table = 'leave_status'
-
-class LeaveType(models.Model):
-    name  = models.CharField(max_length=64)
-    count = models.FloatField()
-
-    class Meta:
-        db_table = 'leave_types'
