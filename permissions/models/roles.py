060cd88783be80b7dd92b27b8e788cddac4d6845
tonic523 <diddmlvkf@gmail.com>
2021-06-14 20:04:53 +0900

    Refactor:1차 코드 리뷰 반영(모델링)

--- permissions/models/roles.py
+++ permissions/models/roles.py
@@ -1,11 +1,9 @@
 from django.db import models
-
-from .roles_permissions import RolePermission
+from django.db.models.deletion import CASCADE
 
 class Role(models.Model):
     name        = models.CharField(max_length=128)
     description = models.TextField(default=None, null=True)
-    permissions = models.ManyToManyField('permissions.Permission', through=RolePermission, related_name="roles")
     
     class Meta:
         db_table = 'roles'
