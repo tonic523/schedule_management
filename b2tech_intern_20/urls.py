771ddec3c719cf8d6cb2b3d29f461127f6ec1531
tonic523 <diddmlvkf@gmail.com>
2021-06-15 16:53:50 +0900

    Add: user api 추가

--- b2tech_intern_20/urls.py
+++ b2tech_intern_20/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('users', include('users.urls.users_url'))
+    path('users', include('users.urls.users_schedules'))
 ]
--- users/urls/users_url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-from users.views.users_view import ListUsers
-
-urlpatterns = [
-    path('', ListUsers.as_view()),
-]
