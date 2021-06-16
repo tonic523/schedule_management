54c93a0a36759316f402f4c1829eafb7da49e86c
tonic523 <diddmlvkf@gmail.com>
2021-06-16 16:29:48 +0900

    Create: requirements.txt

--- b2tech_intern_20/settings.py
+++ b2tech_intern_20/settings.py
@@ -28,10 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'corsheaders',
     'rest_framework',
-
     'rest_framework.authtoken',
+    'drf_yasg',
+
     'users',
     'schedules',
     'permissions',
