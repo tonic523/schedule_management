9c1c98e6bd5c685001e4d45f833f99c0a55137a4
tonic523 <diddmlvkf@gmail.com>
2021-06-17 15:30:23 +0900

    모델 Role field 수정

--- permissions/models/roles.py
+++ permissions/models/roles.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 
 class Role(models.Model):
-    name        = models.CharField(max_length=128)
-    description = models.TextField(default=None, null=True)
+    type = models.CharField(max_length=64)
+    name = models.CharField(max_length=128, default=None, null=True)
     
     class Meta:
         db_table = 'roles'
