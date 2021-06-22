80c037d62dce6964ffde1dba978cc41f1db5b7eb
tonic523 <diddmlvkf@gmail.com>
2021-06-22 14:11:45 +0900

    Refactor: 로그인 시 토큰 반환 방법 수정

--- users/views/login.py
+++ users/views/login.py
@@ -2,20 +2,26 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 #from rest_framework_jwt.utils import jwt_decode_handler
 
 from users.serializers.login import UserLoginSerializer
 
 @api_view(['POST'])
-@permission_classes(['AllowAny'])
+@permission_classes([AllowAny])
 def login(request):
     serializer = UserLoginSerializer(data=request.data)
 
     if not serializer.is_valid(raise_exception=True):
         return Response({"message": "Request Body Error"}, status=status.HTTP_409_CONFLICT)
-    
-    response = {
+    #'decode_token' : jwt_decode_handler(serializer.data['token'])
+
+    response = Response({
+        'message' : 'SUCCESS',
         'token': serializer.data['token'],
-        #'decode_token' : jwt_decode_handler(serializer.data['token'])
-    }
-    return Response(response, status=status.HTTP_200_OK)
+    },
+    headers={'access_token' : serializer.data['token']}, 
+    content_type= 'application/json', 
+    status=status.HTTP_200_OK)
+    
+    return response
