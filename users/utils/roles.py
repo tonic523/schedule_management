19708be84232b63da2567691be36f1279672a7ff
tonic523 <diddmlvkf@gmail.com>
2021-06-22 14:10:53 +0900

    Refactor: get_roles 모듈 수정

--- users/utils/roles.py
+++ users/utils/roles.py
@@ -2,7 +2,7 @@ from permissions.models.users_roles import UserRole
 
 def get_roles(employee_number):
     results = {}
-    roles = UserRole.objects.filter(user__employee_number=employee_number).select_related('role')
-    for role in roles:
-        role.type = role.name
+    user_roles = UserRole.objects.filter(user__employee_number=employee_number).select_related('role')
+    for user_role in user_roles:
+        user_role.role.type = user_role.role.name
     return results
