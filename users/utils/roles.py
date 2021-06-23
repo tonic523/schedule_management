a31a0549c77f360b172ec18dddfb72d8ecf3bb67
tonic523 <diddmlvkf@gmail.com>
2021-06-23 12:32:17 +0900

    Add: User 모델에 토큰 필드 추가

--- users/utils/roles.py
+++ users/utils/roles.py
@@ -3,7 +3,7 @@ from permissions.models import roles_permissions
 from permissions.models.users_roles import UserRole
 from permissions.models.roles_permissions import RolePermission
 from rest_framework.permissions import BasePermission
-from rest_framework_jwt.utils import 
+
 def get_roles(employee_number):
     results = {}
     user_roles = UserRole.objects.filter(user__employee_number=employee_number).select_related('role')
