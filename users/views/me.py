bc890f5cda6cca2c07e494b60d48c04dea0e9e42
Michael <sj.baek@b2groups.com>
2021-06-29 03:05:26 +0000

    Refactor: me endpoint

--- users/views/me.py
+++ users/views/me.py
@@ -1,20 +1,19 @@
 from rest_framework.response import Response
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.views import APIView
 
 from users.utils.token import validate_login
 from users.utils.roles import get_roles
-from users.serializers.login import UserSerializer
+from users.serializers.users import UserSerializer
 class Me(APIView):
     permission_classes = []
     @validate_login
     def get(self, request):
-        employee = request.user
-        serializer = UserSerializer(employee)
-        # data = {
-        #     'name':employee.name,
-        #     'employee_number':employee.employee_number,
-        #     'roles':get_roles(employee.employee_number),
-        #     'remaining_annual_leave':employee.remaining_annual_leave
-        # }
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = request.user
+        serializer = UserSerializer(user)
+        if not serializer.is_valid:
+            return Response("유저 정보가 필요합니다", status=status.HTTP_400_BAD_REQUEST)
+        data = serializer.data
+        data["roles"] = get_roles(user.employee_number)
+        data["leave"] = user.annual_leave.leave_count
+        return Response(data, status=status.HTTP_200_OK)
