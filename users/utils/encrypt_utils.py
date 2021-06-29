59a38f13aecf8b18122008c431dc4dbba35d345a
Michael <sj.baek@b2groups.com>
2021-06-29 03:01:54 +0000

    Refactor: decryption 로직 수정

--- users/utils/encrypt_utils.py
+++ users/utils/encrypt_utils.py
@@ -9,23 +9,28 @@ from Cryptodome.Protocol.KDF import PBKDF2
 
 BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) %  BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+unpad = lambda s: s[:-ord(s[-1])]
+
+def b64encode(string):
+    return base64.b64encode(string).decode("utf-8")
+
+def b64decode(string):
+    return base64.b64decode(string.encode("utf-8"))
 
 def encrypt(information, key):
     key = key.encode('utf-8')
     information = pad(information).encode('utf-8')
     iv = Random.new().read(AES.block_size)
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    result = base64.b64encode(iv + cipher.encrypt(information))
-    print(result)
+    result = b64encode(iv + cipher.encrypt(information))
     return result
 
 def decrypt(information, key):
     key = key.encode('utf-8')
-    information = base64.b64decode(information)
-    iv = information[:16]
+    information = b64decode(information)
+    iv = information[:AES.block_size]
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(information[16:]).decode('utf-8'))
+    return unpad(cipher.decrypt(information[AES.block_size:]).decode('utf-8'))
 
 def encryption(user_input):
     return encrypt(user_input, my_settings.KEY)
